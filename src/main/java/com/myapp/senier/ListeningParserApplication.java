1940eb749447bb27dddee55eafa3b60e590a13b1
ppark2ya <vigabhan1231@gmail.com>
2019-01-18 00:20:14 +0900

    first commit

---
+++ src/main/java/com/myapp/senier/ListeningParserApplication.java
@@ -0,0 +1,14 @@
+package com.myapp.senier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListeningParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListeningParserApplication.class, args);
+	}
+
+}
+
