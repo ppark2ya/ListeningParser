0ca1d3a29fa8cd099d8ccb9e24f30ea58c12077e
ppark2ya <vigabhan1231@gmail.com>
2019-01-19 13:32:01 +0900

    설정 추가

--- src/main/java/com/myapp/senier/ListeningParserApplication.java
+++ src/main/java/com/myapp/senier/ListeningParserApplication.java
@@ -2,7 +2,9 @@ package com.myapp.senier;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class ListeningParserApplication {
 
